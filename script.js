57cbb2fbf42041e50f175c7034ae9d3c21963d90
bigbossthunyaluk <thunyaluk2546.sasiwarinkul@gmail.com>
2021-05-01 13:26:06 +0700

    add MyWebsite

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("CESCa16 Bigboss Thunyaluk Group-G")
+
+document.getElementById("BBtn").addEventListener("click",btnClick);
+
+function btnClick(){
+    window.alert("ขอบคุณสำหรับการแสดงความคิดเห็น");
+} 
